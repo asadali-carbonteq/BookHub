1ddeb9513473b5a14922ae2386c2c212ae5148cc
moghisw <mome6010@gmail.com>
2023-09-30 14:10:11 +0500

    conflict resolved

--- App.js
+++ App.js
@@ -1,14 +1,30 @@
 import React from 'react';
-import { StyleSheet, Text, View, Pressable } from 'react-native';
-import WelcomeScreen from './components/welcome';
-import SignInScreen from './components/signIn';
-import HomeScreen from './components/home';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import WelcomeScreen from './App/components/welcome';
+import SignInScreen from './App/components/signIn';
+import SignupForm from './App/components/signUp';
+import HomeScreen from './App/components/home';
+import WelcomeScreen from './App/components/welcome';
+import SignInScreen from './App/components/signIn';
+import HomeScreen from './App/components/home';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     // <WelcomeScreen />
     // <SignInScreen />
+    // <SignupForm/>
     // <HomeScreen />
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Welcome">
+        <Stack.Screen name="welcome" component={WelcomeScreen} />
+        <Stack.Screen name="Signup" component={SignupForm} />
+        <Stack.Screen name="SignIn" component={SignInScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
